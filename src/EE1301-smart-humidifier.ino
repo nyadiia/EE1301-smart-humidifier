15fb1ddec95118d395cf798a7327478993860e65
Nadia Potteiger <nyadiia@pm.me>
2023-03-29 15:39:18 -0500

    init particle project

---
+++ src/EE1301-smart-humidifier.ino
@@ -0,0 +1,18 @@
+/*
+ * Project EE1301-smart-humidifier
+ * Description: Smart humidifier that automatically adjust to fit the desired level.
+ * Authors: Nadia Potteiger, Maya Bartholomew, Samanta Saucedo, Brenna Dietrich
+ * Date: 3/29/23
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
